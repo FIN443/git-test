0f172aa845c9c7341d564172e2092abba2d1c00c
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-05-06 15:49:56 +0900

    fibo

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
-print("hello world")
+# print("hello world")
 
-for i in range(1, 21):
-    if i % 15 == 0:
-        print("yeardream")
-    elif i % 3 == 0:
-        print("year")
-    elif i % 5 == 0:
-        print("dream")
-    else:
-        print(i)
+# for i in range(1, 21):
+#     if i % 15 == 0:
+#         print("yeardream")
+#     elif i % 3 == 0:
+#         print("year")
+#     elif i % 5 == 0:
+#         print("dream")
+#     else:
+#         print(i)
+
+
+def fibo(x):
+    if x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    return fibo(x - 1) + fibo(x - 2)
+
+
+print(fibo(3))
