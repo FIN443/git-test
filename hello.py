97e681868851f4bef081e223b24536c4260ae167
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-05-06 15:48:03 +0900

    year

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+for i in range(1, 21):
+    if i % 15 == 0:
+        print("yeardream")
+    elif i % 3 == 0:
+        print("year")
+    elif i % 5 == 0:
+        print("dream")
+    else:
+        print(i)
